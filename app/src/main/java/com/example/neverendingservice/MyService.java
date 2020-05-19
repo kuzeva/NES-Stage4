80104cbdf63f6c8f626969eadeb332199a760d16
Bojana Kuzeva <kuzeva01@gmail.com>
2020-05-19 21:45:59 +0200

    stage 3

---
+++ app/src/main/java/com/example/neverendingservice/MyService.java
@@ -0,0 +1,186 @@
+package com.example.neverendingservice;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import com.example.neverendingservice.NetworkTasks.NetworkCheckClass;
+import com.example.neverendingservice.NetworkTasks.NetworkPING;
+import com.example.neverendingservice.Notification.ServiceNotification;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MyService extends Service {
+
+    protected static final int NOT_ID = 150;
+    private static String TAG = "Service";
+
+    private static Service currentService;
+    private int timeCounter ;
+    private static Timer timer;
+    private TimerTask timerTask;
+    public NetworkCheckClass networkCheck;
+    public Context context;
+    public int pingNumber = 0;
+
+    private final static int INTERVAL = 600*1000;
+
+
+
+    public MyService() {
+
+        super();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            restartForeground();
+        }
+        currentService = this;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+
+       // Log.d(TAG , "Restarting service  ");
+        networkCheck = new NetworkCheckClass(this);
+        boolean network = networkCheck.networkCheck();
+
+        if(network){
+            Log.i("MAKE_PING","START_COMAND: Connected to the internet");
+            Thread t = new Thread(){
+                public void run(){
+                    taskDelayLoop();
+                }
+            };
+            t.start();
+        }else{
+            Log.i("START_COMMAND","NOT connected to the internet");
+        }
+
+        Log.i("MAKE_PING","START_COMMAND: The onStartCommand() is called");
+
+        SharedPreferences prefs= getSharedPreferences("com.example.neverendingservice.ActiveServiceRunning", MODE_PRIVATE);
+
+        if(prefs.getInt("timeCounter",0)!=0){
+            timeCounter = prefs.getInt("counter",0);
+        }
+
+        if (intent == null){
+            HelperLaunchServiceClass.launchService(this);
+        }
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            restartForeground();
+        }
+
+        startTimer();
+
+        return START_STICKY;
+    }
+
+    public void taskDelayLoop(){
+        while(true){
+
+            new NetworkPING(getApplicationContext()).execute();
+            Log.i("MAKE_PING","doInBackground() starts");
+            try{
+                Thread.sleep(INTERVAL);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private void startTimer() {
+        Log.i(TAG , "Starting timer");
+
+        if(timer != null){
+            timer.cancel();
+            timer = null;
+        }
+
+        timer = new Timer();
+
+        initializeTimerTask();
+
+        Log.i("SERVICE", "Scheduling...");
+        timer.schedule(timerTask, 1000, 1000);
+    }
+
+    private void initializeTimerTask() {
+        Log.i("SERVICE", "initialising TimerTask");
+        timerTask = new TimerTask() {
+            public void run() {
+                Log.i("in timer", "in timer ++++  " + (timeCounter++));
+            }
+        };
+    }
+
+    private void restartForeground() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            try {
+                ServiceNotification notification = new ServiceNotification();
+                startForeground(NOT_ID,notification.setNotification(this, "Service notification", "This is the service's notification",R.drawable.ic_android));
+                Log.i(TAG,"Restarting foreground successful");
+                startTimer();
+            }catch (Exception e){
+                Log.e(TAG , "Error in notification " + e.getMessage());
+            }
+        }
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        //Log.d(TAG,"Service destroyed");
+
+        try {
+            SharedPreferences prefs= getSharedPreferences("com.example.neverendingservice.ActiveServiceRunning", MODE_PRIVATE);
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putInt("timeCounter", timeCounter);
+            editor.apply();
+
+        } catch (NullPointerException e) {
+            Log.e("SERVER", "Error " +e.getMessage());
+
+        }
+
+        Intent broadcastIntent = new Intent("com.example.neverendingservice");
+        sendBroadcast(broadcastIntent);
+
+        if(timer != null){
+            timer.cancel();
+            timer = null;
+        }
+    }
+
+    @Override
+    public void onTaskRemoved(Intent rootIntent) {
+        super.onTaskRemoved(rootIntent);
+
+        Log.i(TAG , "onTaskRemoved called");
+
+        Intent broadcastIntent = new Intent("com.example.neverendingservice");
+        sendBroadcast(broadcastIntent);
+    }
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+         return null;
+    }
+}
---
+++ app/src/main/java/com/example/neverendingservice/NetworkTasks/NetworkPING.java
@@ -0,0 +1,126 @@
+package com.example.neverendingservice.NetworkTasks;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static android.content.Context.MODE_PRIVATE;
+
+public class NetworkPING extends AsyncTask<Void,Void,String> {
+
+    public static String ping;
+    public String host;
+    public int packetSize;
+    public int jobPeriod;
+    public BufferedReader input;
+    public int count;
+    public  String line;
+
+    public String resultOk;
+    public NetworkCheckClass networkCheck;
+    public Context mContext;
+    private SharedPreferences preferences;
+
+    public NetworkPING(Context context) {
+        mContext=context;
+    }
+
+
+    @Override
+    protected String doInBackground(Void... voids) {
+
+        networkCheck = new NetworkCheckClass(mContext.getApplicationContext());
+
+
+        try {
+            String jsonData = getJSON.getBackendJson();
+            JSONArray jsonArray = new JSONArray(jsonData);
+
+            for(int i =0; i<jsonArray.length();i++){
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                host = jsonObject.getString("host");
+                count = jsonObject.getInt("count");
+                packetSize = jsonObject.getInt("packetSize");
+                jobPeriod = jsonObject.getInt("jobPeriod");
+
+                for(int j=0; j<=600/jobPeriod;j++){
+                    ping = makePING(host,count,packetSize);
+
+                    Log.i("MAKE_PING",ping);
+                    try{
+                        Thread.sleep(jobPeriod*1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return ping;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
+
+    public String makePING(String Host , int Count, int PacketSize){
+        String Ping = "";
+
+        try{
+
+            String pingCmd = "ping -s " + PacketSize + " -c "+ Count + " " + Host;
+            Runtime runtime = Runtime.getRuntime();
+            Process process = runtime.exec(pingCmd);
+            input = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            line = " ";
+            while ((line = input.readLine()) != null){
+                Ping += line;
+
+            }
+            input.close();
+
+            Log.i("MAKE_PING",Ping);
+            preferences = mContext.getSharedPreferences("com.example.qoscheckapp.ActiveServiceRunning", MODE_PRIVATE);
+            SharedPreferences.Editor editor = preferences.edit();
+            if(networkCheck.networkCheck()){
+                postJSON.postBackendJson(Ping);
+                if(preferences.getString("result1",null)!= null){
+                    postJSON.postBackendJson(preferences.getString("result1",null));
+                    editor.putString("result1",null);
+                }
+                if(preferences.getString("result2",null)!= null){
+                    postJSON.postBackendJson(preferences.getString("result2",null));
+                    editor.putString("result2",null);
+                }
+                else{
+                    if((preferences.getString("result1",null)!=null ) && (preferences.getString("result1",null)!=null)
+                            || (preferences.getString("result1",null)==null ) && (preferences.getString("result1",null)==null) ){
+                        editor.putString("result1",Ping);
+                    }else {
+                        editor.putString("result2",Ping);
+                    }
+                    editor.apply();
+                }
+
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return Ping;
+    }
+}
